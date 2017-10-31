d54885bf560705a18b1aec2224d3b571ee906eb8
Jeff Miller <jmiller@enova.com>
2017-10-31 11:47:59 -0500

    First attempted redesign.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$( document ).ready(function() {
+  $("#rss-feed").rss("http://us14.campaign-archive.com/feed?u=94b3666801afda9726b39f45b&id=4e2c69e92a",
+    {
+      entryTemplate:'<li><a href="{url}" target="_blank">{teaserImage}</a>&nbsp;&nbsp;<a href="{url}" target="_blank">{title}</a></li>'
+  });
+});
